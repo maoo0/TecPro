2a2ae5096d541d6c2c523f9379cfc6d458342541
Mao <maumantegazzamori@gmail.com>
2025-10-09 03:54:26 -0500

    Add files via upload

---
+++ Lab 05 (completo)/2025-2_Lab05_Arreglos_Ordenados/Bibliotecas/Utils.h
@@ -0,0 +1,17 @@
+//
+// Created by mauri on 06/10/2025.
+//
+
+#ifndef INC_2025_2_LAB05_ARREGLOS_ORDENADOS_UTILS_H
+#define INC_2025_2_LAB05_ARREGLOS_ORDENADOS_UTILS_H
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#define anchoDoc 120
+#define maxCanales 120
+#define maxEtiquetas 120
+#define maxExC 600
+using namespace std;
+
+#endif //INC_2025_2_LAB05_ARREGLOS_ORDENADOS_UTILS_H
